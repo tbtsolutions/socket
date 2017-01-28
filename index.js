149987149a31c1e223d11685dc5f37e63bf1c6e7
tbtsolutions <ibrahim@turuncbt.com>
2017-01-28 15:09:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
    socket.on('objectreceived',function(data){
-      io.sockets.emit('objectmessage', data);
+      io.sockets.emit('objectmessage');
     });
 });
 
